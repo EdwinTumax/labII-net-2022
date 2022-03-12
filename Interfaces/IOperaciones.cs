3d78b59baec08ac8ea77c81100a09ede8ddb78af
Edwin Tumax <edwintumax@gmail.com>
2022-03-12 11:15:46 -0600

    Solución del laboratorio II

---
+++ Interfaces/IOperaciones.cs
@@ -0,0 +1,8 @@
+namespace TestExample.Interfaces
+{
+    public interface IOperaciones
+    {
+        public void ListarMisDatos(string identificador);
+        public bool EliminarAsignatura(string asignatura);
+    }
+}
